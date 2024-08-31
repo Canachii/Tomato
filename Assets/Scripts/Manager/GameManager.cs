15274db246db71c7c85b442ac0b38777f9a0c518
gudals123 <104682516+gudals123@users.noreply.github.com>
2024-08-31 18:20:50 +0900

    Popup,Manager 추가
    1. UI_Popup추가
    2. Manager 추가

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+
+    public void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+    public void GameOver()
+    {
+
+    }
+
+    public void GameClear()
+    {
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/Manager/Managers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using static Define;
+
+public class Managers : MonoBehaviour
+{
+    public static Managers Instance { get; private set; }
+
+    //private static DataManager dataManager = new();//
+    private static UIManager uiManager = new();
+    private static ResourceManager resourceManager = new();
+    private static GameManager gameManager = new();
+    private static SoundManager soundManager = new SoundManager();
+
+    //public static DataManager Data { get { Init(); return dataManager; } }
+    public static UIManager UI { get { Init(); return uiManager; } }
+    public static ResourceManager Resource { get { Init(); return resourceManager; } }
+    public static GameManager Game { get { Init(); return gameManager; } }
+    public static SoundManager Sound { get { Init(); return soundManager; } }
+
+    void Start()
+    {
+        Init();
+    }
+
+    private static void Init()
+    {
+        if (Instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+                go = new GameObject { name = "@Managers" };
+
+            Instance = Utils.GetOrAddComponent<Managers>(go);
+            DontDestroyOnLoad(go);
+
+            gameManager.Init();
+            soundManager.Init();
+
+            Application.targetFrameRate = 60;
+        }
+    }
+}
